56918c6edb9dd6d35dac9d3497d716d2940a0de5
Shen-Codes <60067978+Shen-Codes@users.noreply.github.com>
2020-07-23 10:26:45 -0400

    fixed https route for api call

--- src/components/Account/NewsArchiver/api/story.js
+++ src/components/Account/NewsArchiver/api/story.js
@@ -1,9 +1,6 @@
-const HN_BASE_URL = 'http://hn.algolia.com/api/v1/search?query=';
+const HN_BASE_URL = 'https://hn.algolia.com/api/v1/search?query=';
 
 const fetchStories = query =>
-	fetch(HN_BASE_URL + query)
-		.then(response => response.json());
+   fetch(HN_BASE_URL + query).then(response => response.json());
 
-export {
-	fetchStories,
-};
+export { fetchStories };
